b148fce76f2ed98f84dd888fe81b6eddb3f5a023
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-09-13 16:36:23 +0200

    implement navbar

--- tc_client/src/App.js
+++ tc_client/src/App.js
@@ -2,15 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
+import AppNavbar from "./components/AppNavbar";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/registration" element={<Registration />} />
-    </Routes>
+      <AppNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
     </>
   );
 }
