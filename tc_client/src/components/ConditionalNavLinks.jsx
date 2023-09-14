9e217eb4b220f165ebe8df38fb88f4d0f8d5b40a
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-09-14 23:45:37 +0200

    simplified Conditional navbar component

--- tc_client/src/components/ConditionalNavLinks.jsx
+++ tc_client/src/components/ConditionalNavLinks.jsx
@@ -2,39 +2,35 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import { NavDropdown } from 'react-bootstrap';
 import LogoutButton from "./LogoutButton";
-import homeIcon from '../media/profile.png';
+import profileIcon from '../media/profile.png';
 
 export default function ConditionalNavLinks({ user, setUser }) {
-    if (user) {
-        return (
-            <NavDropdown
-                title={<img src={homeIcon}
-                    alt="profile home icon"
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                />}
-                id="basic-nav-dropdown"
-            >
-                <NavDropdown.Item as={Link} to="/registration" >Registration</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/login">Login</NavDropdown.Item>
-                <NavDropdown.Item ><LogoutButton setUser={setUser} /></NavDropdown.Item>
-            </NavDropdown>
-        );
-    } else {
-        return (
-            <NavDropdown
-                title={<img src={homeIcon}
-                    alt="profile home icon"
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                />}
-                id="basic-nav-dropdown"
-            >
-                <NavDropdown.Item as={Link} to="/registration" >Registration</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/login">Login</NavDropdown.Item>
-            </NavDropdown>
-        );
-    }
+    return (
+        <NavDropdown
+            title={<img src={profileIcon}
+                alt="profile home icon"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+            />}
+            id="basic-nav-dropdown"
+        >
+
+            {!user && (
+                <>
+                    <NavDropdown.Item as={Link} to="/registration" >Sign Up</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/login">Sign In</NavDropdown.Item>
+                </>
+            )}
+            {user && (
+                <>
+                    <NavDropdown.Item as={Link} to="/" >Profile</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/" >Settings</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/" >My Tours</NavDropdown.Item>
+                    <NavDropdown.Item ><LogoutButton setUser={setUser} /></NavDropdown.Item>
+                </>
+            )}
+        </NavDropdown>
+    );
+
 }
