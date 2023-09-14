fc5a580a51ef66f31741ed1b80ae21ecbf26f79b
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-09-14 15:32:26 +0200

    place footer and set user info

--- tc_client/src/App.js
+++ tc_client/src/App.js
@@ -1,19 +1,25 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import AppNavbar from "./components/AppNavbar";
 import './App.css';
+import User from "./components/User";
+import Footer from "./components/Footer";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="App">
-      <AppNavbar />
+      <AppNavbar user={user} setUser={setUser} />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
         <Route path="/registration" element={<Registration />} />
+        <Route path="/user/:username" element={<User />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
