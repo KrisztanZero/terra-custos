544682e325904ab49d5609ada737fb99ac4fa0e6
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-09-14 15:32:50 +0200

    update home

--- tc_client/src/pages/Home.jsx
+++ tc_client/src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import Tours from "../components/Tours";
+
 export default function Home() {
     return (
         <div className="container">
-            <h1>HomePage</h1>
+            <p className="container mt-5">TerraCustos is a passionately developed application that connects people's collective efforts in nature conservation and sustainability.
+                The app aims to inspire and encourage people to protect and care for our Earth, while enabling them to actively participate in preserving the environment, all while fostering a closer connection to nature through the experiences gained during their journeys.</p>
+            <Tours />
         </div>
     )
 }
