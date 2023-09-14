1e0aa31ac96bdc8af9c11c4febc0226a7f783ac2
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-09-14 23:44:50 +0200

    update user component

--- tc_client/src/components/User.jsx
+++ tc_client/src/components/User.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getUserByUsername } from '../services/userService';
-import { Card, Container } from 'react-bootstrap';
+import { Card, Container, Alert, Spinner } from 'react-bootstrap';
 
 export default function User() {
     const { username } = useParams();
     const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -17,25 +18,40 @@ export default function User() {
                 setUser(null);
             } finally {
                 setTimeout(() => {
-                    setUser(null);
-                }, 20000);
+                    setIsLoading(false);
+                }, 1500);
             }
         };
 
         fetchUser();
     }, [username]);
 
+    if (isLoading) {
+        return (
+            <Container className="text-center mt-5">
+                <Spinner animation="border" role="status">
+                    <span className="sr-only"></span>
+                </Spinner>
+                <p className='text-success my-3 fs-5'>Loading...</p>
+            </Container>
+        );
+    }
+
     if (!user) {
-        return <div>Loading...</div>;
+        return (
+            <Container className="text-center">
+                <Alert variant="danger" className='my-3 fs-5'>User not found.</Alert>
+            </Container>
+        );
     }
 
     return (
         <Container>
-            <h2>User Profile: {user.username}</h2>
-            <Card>
+            <Card className='w-50  mx-auto m-5'>
                 <Card.Body>
                     <Card.Title>{user.username}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">{user.email}</Card.Subtitle>
+                    <Card.Subtitle className="m-3 text-muted">{user.email}</Card.Subtitle>
+                    <Card.Text className="m-3 text-muted">{user.description}</Card.Text>
                 </Card.Body>
             </Card>
         </Container>
