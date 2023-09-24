a98f8f24c72f16cc316af789ada2a653cd50dcf8
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-09-24 22:15:55 +0200

    add greeting controller

---
+++ terra-custos-api/terra-custos-web-api/Controllers/GreetingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace terra_custos_web_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GreetingController : Controller
+    {
+        [HttpGet]
+        public IActionResult Greeting()
+        {
+            return Ok("Welcome to Terra Custos web page!");
+        }
+    }
+}
