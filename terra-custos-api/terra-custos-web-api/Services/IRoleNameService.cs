f25c672dfc032d7d65a54e976e39e1e5e060149b
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-09-27 00:44:26 +0200

    create role service

---
+++ terra-custos-api/terra-custos-web-api/Services/IRoleNameService.cs
@@ -0,0 +1,9 @@
+﻿using terra_custos_web_api.Models;
+
+namespace terra_custos_web_api.Services
+{
+    public interface IRoleNameService
+    {
+        Task<UserRole> GetRoleByName(string roleName);
+    }
+}
---
+++ terra-custos-api/terra-custos-web-api/Services/RoleNameService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Tokens;
+using terra_custos_web_api.DbContexts;
+using terra_custos_web_api.Models;
+
+namespace terra_custos_web_api.Services
+{
+    public class RoleNameService : IRoleNameService
+    {
+        private readonly TerraCustosContext _context;
+
+        public RoleNameService(TerraCustosContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserRole> GetRoleByName(string roleName)
+        {
+            var role = await _context.Set<UserRole>().FirstOrDefaultAsync(role => role.Name == roleName);
+            if (role == null)
+            {
+                throw new Exception($"Role with the name: {roleName} does not found.");
+            }
+            return role;
+        }
+    }
+}
