1b49d095178bc469a2a5b6bd9fc0a6d734b441de
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-09-27 00:43:39 +0200

    create user data validator

---
+++ terra-custos-api/terra-custos-web-api/Services/UserValidator.cs
@@ -0,0 +1,41 @@
+﻿using terra_custos_web_api.Models;
+
+namespace terra_custos_web_api.Services
+{
+    public class UserValidator
+    {
+        public bool IsValidUser(User user)
+        {
+            if (user == null)
+            {
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(user.Name)
+            {
+                return false;
+            }
+            if (!IsValidEmail(user.Email))
+            {
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(user.Password))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
