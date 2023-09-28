eba6c37cab2aca87954647fce0ed5d226f2da97d
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-09-28 17:28:18 +0200

    create AuthController

---
+++ terra-custos-api/terra-custos-web-api/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using terra_custos_web_api.Models;
+using terra_custos_web_api.Services;
+
+namespace terra_custos_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterUser(LoginUser user)
+        {
+            if (await _authService.RegisterUser(user))
+            {
+                return Ok("Signup Success");
+            }
+            return BadRequest("Something went wrong");
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginUser user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (await _authService.Login(user))
+            {
+                var tokenString = _authService.GenerateTokenString(user);
+                return Ok($"Login Succesfull with token: {tokenString}");
+            }
+            return BadRequest();
+        }
+    }
+}
