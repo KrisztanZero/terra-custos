ae37935336aa5620f892a353591f3878860552a4
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-09-28 14:45:01 +0200

    create Tour.cs

---
+++ terra-custos-api/terra-custos-web-api/Models/Tour.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace terra_custos_web_api.Models
+{
+    public class Tour
+    {
+        [Key]
+        public int TourId { get; set; }
+        public string TourName { get; set; }
+        public string TourDescription { get; set;}
+        public string TourType { get; set;}
+    }
+}
