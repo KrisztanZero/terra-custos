ec6f659d4a07e5edc7e86f74532001bb9578cbba
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-09-28 14:44:45 +0200

    move IRepository.cs

--- terra-custos-api/terra-custos-web-api/IRepositories/IRepository.cs
+++ terra-custos-api/terra-custos-web-api/IRepositories/IRepository.cs
@@ -1,11 +1,11 @@
-﻿namespace terra_custos_web_api.Repositories
+﻿namespace terra_custos_web_api.IRepositories
 {
-    public interface IRepository<T> 
+    public interface IRepository<T>
     {
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
         Task<T> CreateAsync(T entity);
-        Task<T> UpdateAsync(int id,T entity);
+        Task<T> UpdateAsync(int id, T entity);
         Task<bool> DeleteAsync(int id);
     }
 }
