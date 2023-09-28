a4dc18c8ebe047cd9225c66256b1cdf8af6d4228
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-09-28 14:49:35 +0200

    use IdentityDbContext, add Tour to context

--- terra-custos-api/terra-custos-web-api/DbContexts/TerraCustosContext.cs
+++ terra-custos-api/terra-custos-web-api/DbContexts/TerraCustosContext.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using terra_custos_web_api.Models;
 
 namespace terra_custos_web_api.DbContexts
 {
-    public class TerraCustosContext : DbContext
+    public class TerraCustosContext : IdentityDbContext
     {
-        public TerraCustosContext(DbContextOptions<TerraCustosContext> options) : base(options) { }
+        public TerraCustosContext(DbContextOptions options) : base(options) { }
         public DbSet<User> Users { get; set; }
-        public DbSet<UserRole> Roles { get; set; }
+        public DbSet<Tour> Tours { get; set; }
     }
 }
