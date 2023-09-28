a1ead8d414e9179956d0f88d6f6cbef6357437aa
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-09-28 14:44:06 +0200

    remove irrelevant items

--- terra-custos-api/terra-custos-web-api/Services/IRoleNameService.cs
+++
@@ -1,9 +0,0 @@
-﻿using terra_custos_web_api.Models;
-
-namespace terra_custos_web_api.Services
-{
-    public interface IRoleNameService
-    {
-        Task<UserRole> GetRoleByName(string roleName);
-    }
-}
--- terra-custos-api/terra-custos-web-api/Services/IRoleService.cs
+++
@@ -1,9 +0,0 @@
-﻿using terra_custos_web_api.Models;
-using terra_custos_web_api.Repositories;
-
-namespace terra_custos_web_api.Services
-{
-    public interface IRoleService : IRepository<UserRole>
-    {
-    }
-}
