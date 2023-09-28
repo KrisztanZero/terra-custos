cae6c87b9f63e38d53e61c515cda712694ba8a47
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-09-28 17:28:02 +0200

    create AuthService

---
+++ terra-custos-api/terra-custos-web-api/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using terra_custos_web_api.Models;
+
+namespace terra_custos_web_api.Services
+{
+    public interface IAuthService
+    {
+        string GenerateTokenString(LoginUser user);
+        Task<bool> Login(LoginUser user);
+        Task<bool> RegisterUser(LoginUser user);
+    }
+}
