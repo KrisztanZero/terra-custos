2e1a3e197c6e274c3ff5f481eaabf3e031789683
Krisztian Molnar <111390697+KrisztanZero@users.noreply.github.com>
2023-10-12 17:04:01 +0200

    Delete terra-custos-api/terra-custos-web-api directory

--- terra-custos-api/terra-custos-web-api/Models/LoginUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace terra_custos_web_api.Models
-{
-    public class LoginUser
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
--- terra-custos-api/terra-custos-web-api/Services/ITourService.cs
+++
@@ -1,9 +0,0 @@
-﻿using terra_custos_web_api.IRepositories;
-using terra_custos_web_api.Models;
-
-namespace terra_custos_web_api.Services
-{
-    public interface ITourService : IGenericRepository<Tour>
-    {
-    }
-}
